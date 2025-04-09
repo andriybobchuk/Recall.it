450b6b8394154a7fbf87f7e7654529702a24fa99
Andrii Bobchuk <andriybobchuk@gmail.com>
2025-04-09 23:51:05 +0200

    Delete unused classes

--- composeApp/src/iosMain/kotlin/com/recallit/di/ViewModelModule.kt
+++ composeApp/src/iosMain/kotlin/com/recallit/di/ViewModelModule.kt
@@ -3,13 +3,10 @@ package com.recallit.di
 import com.recallit.data.repository.DummyCoreRepositoryImpl
 import com.recallit.domain.repository.CoreRepository
 import com.recallit.presentation.cards_screen.CardsViewModel
-import com.recallit.presentation.packs_screen.MainViewModel
 import com.recallit.presentation.packs_screen.PacksViewModel
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 actual val viewModelModule = module {
-    singleOf(::MainViewModel)
     single { PacksViewModel(repository = get()) }
     single { CardsViewModel(repository = get()) }
 
